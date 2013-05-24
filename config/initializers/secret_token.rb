80c700c2d0e5a74d3691c9a8da1e4369d534595c
root <root@aztecapps.(none)>
2013-05-24 07:07:02 +0000

    Initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HydroFlask::Application.config.secret_token = 'cf7fd1fae3e5696bd331476efebced60e2f9ae0c1731ccb65245dfc98149a0a325dee4962c0cfcdf24ec0bd0b6ff4e90ea69a0644d0c9263f36c8dad33a4ba72'
