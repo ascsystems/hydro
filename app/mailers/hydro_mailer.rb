4a71c0e5ce1d55c395a6832eeab7cea08962f163
Greg Norris <gregnorris@tactica.ca>
2013-08-21 23:40:01 -0500

    Adding some Mandrill SMTP mailer config to development env file, and the start to some form field changes to the Order view page

---
+++ app/mailers/hydro_mailer.rb
@@ -0,0 +1,10 @@
+class HydroMailer < ActionMailer::Base
+  
+  #FIXME: finish this
+  def purchase_notification_email(order)
+    #@user = order.customer
+    #@order = order
+    #mail(to: "david@aztec-apps.com", from: "notifier@hydroflask.com", subject: "Order Notification - Order ID: #{order.id}")
+  end
+  
+end
