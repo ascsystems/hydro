7532b3beb9d71ccdaa89ff65bac7ad69234ab67a
Greg Norris <greg7norris@gmail.com>
2013-08-27 16:25:43 -0500

    Making table creation conditional for shippings and state_tax_rate - because the migration messed up on the server

--- db/migrate/20130819114236_create_shippings.rb
+++ db/migrate/20130819114236_create_shippings.rb
@@ -1,9 +1,15 @@
 class CreateShippings < ActiveRecord::Migration
   def change
-    create_table :shippings do |t|
-      t.string :display_text
-
-      t.timestamps
+    
+    # order go botched up on the server, so I need to make this conditional
+    if ActiveRecord::Base.connection.tables.include?(:shippings)
+    
+      create_table :shippings do |t|
+        t.string :display_text
+  
+        t.timestamps
+      end
     end
+    
   end
 end
