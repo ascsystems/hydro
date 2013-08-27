92309109654f57a92c9a22655508540f8edc2c30
Greg Norris <greg7norris@gmail.com>
2013-08-27 16:35:21 -0500

    reversing the migration conditional changes -- had to drop the tables manually on the server anyway

--- db/migrate/20130819114236_create_shippings.rb
+++ db/migrate/20130819114236_create_shippings.rb
@@ -2,14 +2,14 @@ class CreateShippings < ActiveRecord::Migration
   def change
     
     # order go botched up on the server, so I need to make this conditional
-    if ActiveRecord::Base.connection.tables.include?(:shippings)
+    #if ActiveRecord::Base.connection.tables.include?(:shippings)
     
       create_table :shippings do |t|
         t.string :display_text
   
         t.timestamps
       end
-    end
+    #end
     
   end
 end
