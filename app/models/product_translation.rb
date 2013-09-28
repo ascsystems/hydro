605988f3a0e89aa1be46ea414567acfa465b9d6c
david <david@ascsystems.com>
2013-09-27 22:21:05 -0400

    threshholds

--- app/models/product_translation.rb
+++ app/models/product_translation.rb
@@ -1,5 +1,5 @@
 class ProductTranslation < ActiveRecord::Base
-  attr_accessible :description, :price, :sku, :netsuite_id, :quantity, :weight
+  attr_accessible :description, :price, :sku, :netsuite_id, :quantity, :weight, :threshhold
 
   has_many :product_images
 
