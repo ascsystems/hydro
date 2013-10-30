c0540572f02aee22e27541b755e4f4b2e6bafd9f
david <david@ascsystems.com>
2013-10-30 11:58:36 -0400

    temporary shipping fix

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -22,7 +22,7 @@ class Shipping < ActiveRecord::Base
   	response.rates.each do |rate|
       shipping_types.each do |type|
         if rate.service_name == type[:key_text]
-  		    rates.push({id: type[:id], name: type[:display_text], price: (rate.total_price * 0.01)})
+  		    rates.push({id: type[:id], name: type[:display_text], price: (rate.total_price * 0.011)})
         end
       end
   	end
