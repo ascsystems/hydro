fa2d178cba67c52683716305181b6a1ed23530ee
david <david@ascsystems.com>
2013-11-07 11:58:11 -0500

    Admin changes

--- app/admin/product_image.rb
+++ app/admin/product_image.rb
@@ -1,3 +1,18 @@
 ActiveAdmin.register ProductImage do
-
+  form do |f|
+    #column "Product" do |product|
+    #  link_to product.name, admin_product_path(product)
+    #end
+    f.inputs "Product Image" do
+      f.input :product_id, as: :select, collection: Product.all.map {|p| [p.name, p.id]}, include_blank: false
+      f.input :path
+      f.input :name
+      f.input :default_image
+      f.input :product_translation_id, as: :select, collection: ProductTranslation.all.map {|pt| ["#{pt.sku} - #{pt.description}", pt.id]}, include_blank: false
+    end
+    # :product_translation_id do |product_translation|
+    #  link_to "#{product_translation.sku} - #{product_translation.description}", admin_product_translation_path(product_translation)
+    #end
+    f.actions
+  end
 end
---
+++ app/admin/product_option_value_image.rb
@@ -0,0 +1,8 @@
+ActiveAdmin.register ProductOptionValueImage do
+  form do |f|
+    f.inputs "Product Option Value Image" do
+      f.input :product_image_id, as: :select, collection: ProductImage.all.map {|pi| [pi.name, pi.id]}, include_blank: false
+      f.input :product_option_value_id, as: :select, collection: ProductOptionValue.all.map {|pov| [pov.option_value.name, pov.id]}, include_blank: false
+    end
+  end
+end
