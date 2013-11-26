5473376d814097c108ab30f11b09bb2f8866cf62
david <david@ascsystems.com>
2013-11-26 18:07:59 -0500

    email confirmation

--- app/mailers/hydro_mailer.rb
+++ app/mailers/hydro_mailer.rb
@@ -1,10 +1,9 @@
 class HydroMailer < ActionMailer::Base
   
-  #FIXME: finish this
   def purchase_notification_email(order)
     #@user = order.customer
-    #@order = order
-    #mail(to: "david@aztec-apps.com", from: "notifier@hydroflask.com", subject: "Order Notification - Order ID: #{order.id}")
+    @order = order
+    mail(to: "david@aztec-apps.com", from: "notifier@hydroflask.com", subject: "Hydro Flask Order Notification - Order ID: #{order.invoice_number}")
   end
   
 end
