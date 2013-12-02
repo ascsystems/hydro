315c035b85e4472e22889ef3363dc58d305e1c71
david <david@ascsystems.com>
2013-12-02 16:25:35 -0500

    added netsuite id to form

--- app/admin/product_translation.rb
+++ app/admin/product_translation.rb
@@ -1,3 +1,19 @@
 ActiveAdmin.register ProductTranslation do
-
+  form do |f|
+    #column "Product" do |product|
+    #  link_to product.name, admin_product_path(product)
+    #end
+    f.inputs "Product Translation" do
+      f.input :sku
+      f.input :description
+      f.input :price
+      f.input :quantity
+      f.input :threshhold
+      f.input :netsuite_id
+    end
+    # :product_translation_id do |product_translation|
+    #  link_to "#{product_translation.sku} - #{product_translation.description}", admin_product_translation_path(product_translation)
+    #end
+    f.actions
+  end
 end
