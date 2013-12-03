7674fe4e9b8752d2ee63ecced5e3d1ccb683b5a4
david <david@ascsystems.com>
2013-12-03 18:21:17 -0500

    fix for order page

--- app/admin/order.rb
+++ app/admin/order.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Order do
-
+	filter :email
+	filter :first_name
+	filter :last_name
+	filter :address
+	filter :city
+	filter :state
+	filter :zip
+	filter :created_at
+	filter :invoice_number
+	filter :payment_total_cost
+	filter :netsuite_status
 end
